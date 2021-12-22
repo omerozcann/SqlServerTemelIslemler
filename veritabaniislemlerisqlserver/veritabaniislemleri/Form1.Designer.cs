45c0fc422fb0739ede07e704e2440add5c3ee287
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2021-12-22 17:53:04 +0300

    .

---
+++ veritabaniislemlerisqlserver/veritabaniislemleri/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace veritabaniislemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adisoyaditxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.teltxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.epostatxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.guncelteltxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adisoyaditxt
+            // 
+            this.adisoyaditxt.Location = new System.Drawing.Point(75, 18);
+            this.adisoyaditxt.Name = "adisoyaditxt";
+            this.adisoyaditxt.Size = new System.Drawing.Size(100, 20);
+            this.adisoyaditxt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Adı Soyadı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Telefon";
+            // 
+            // teltxt
+            // 
+            this.teltxt.Location = new System.Drawing.Point(75, 44);
+            this.teltxt.Name = "teltxt";
+            this.teltxt.Size = new System.Drawing.Size(100, 20);
+            this.teltxt.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Eposta";
+            // 
+            // epostatxt
+            // 
+            this.epostatxt.Location = new System.Drawing.Point(75, 70);
+            this.epostatxt.Name = "epostatxt";
+            this.epostatxt.Size = new System.Drawing.Size(100, 20);
+            this.epostatxt.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(271, 146);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // guncelteltxt
+            // 
+            this.guncelteltxt.Location = new System.Drawing.Point(75, 96);
+            this.guncelteltxt.Name = "guncelteltxt";
+            this.guncelteltxt.Size = new System.Drawing.Size(100, 20);
+            this.guncelteltxt.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Güncel Tel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 283);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.guncelteltxt);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.epostatxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.teltxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adisoyaditxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox adisoyaditxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox teltxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox epostatxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox guncelteltxt;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ veritabaniislemlerisqlserver/veritabaniislemleri/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace veritabaniislemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "Server=DESKTOP-JSM6GM2\\SQLEXPRESS;Database=veri;Trusted_Connection=True;";
+            scon.Open();
+            String sql = "insert into teldefter values('"+adisoyaditxt.Text+"','"+teltxt.Text+"','"+epostatxt.Text+"')";
+            SqlCommand scommand = new SqlCommand(sql, scon);
+            scommand.ExecuteNonQuery();
+            scon.Close();           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "Server=DESKTOP-JSM6GM2\\SQLEXPRESS;Database=veri;Trusted_Connection=True;";
+            scon.Open();
+            String sql = "select * from teldefter order by adsoyad asc";
+            SqlDataAdapter sda = new SqlDataAdapter(sql, scon);
+            DataSet ds = new DataSet();//Tabloların dizisini saklayabilen dizi
+            sda.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            scon.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "Server=DESKTOP-JSM6GM2\\SQLEXPRESS;Database=veri;Trusted_Connection=True;";
+            scon.Open();
+            String sql = "delete from teldefter where adsoyad='" + adisoyaditxt.Text + "'";            
+            SqlCommand scommand = new SqlCommand(sql, scon);
+            scommand.ExecuteNonQuery();
+            scon.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "Server=DESKTOP-JSM6GM2\\SQLEXPRESS;Database=veri;Trusted_Connection=True;";
+            scon.Open();
+            String sql = "update teldefter set adsoyad='"+adisoyaditxt.Text+"',telefon='"+guncelteltxt.Text+"',eposta='"+epostatxt.Text+"' where telefon='"+teltxt.Text+"'";
+            SqlCommand scommand = new SqlCommand(sql, scon);
+            scommand.ExecuteNonQuery();
+            scon.Close();
+        }
+    }
+}
